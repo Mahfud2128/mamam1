216f7b3ad178c30b08a522afed64614b45633897
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-28 07:56:37 +0700

    Create cert.sh

---
+++ cert.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS http://griya-digital.my.id:81/ipvps > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS http://griya-digital.my.id:81/ipvps | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS http://griya-digital.my.id:81/ipvps | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+clear
+echo start
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain="$(cat /etc/v2ray/domain)"
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+
+cd /root/
+cd .acme.sh
+bash acme.sh --register-account -m mahfudpr21@gmail.com
+bash acme.sh --issue --standalone -d $domain --force --ecc
+bash acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/openvpn restart
+/etc/init.d/fail2ban restart
+/etc/init.d/cron restart
+/etc/init.d/nginx restart
+#/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
+systemctl restart v2ray@none
+systemctl restart v2ray
+systemctl restart trojan
+systemctl restart vmess-grpc
+systemctl restart vless-grpc
+systemctl restart v2ray@vless
+systemctl restart v2ray@vnone
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "          Service/s Restarted         "
+                echo -e ""
+                echo -e "======================================"
+                exit
+clear 
+./.profile
