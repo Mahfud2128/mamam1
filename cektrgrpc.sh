597acf3469abd9411161210c69599b7520ca6802
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-07-11 19:50:17 +0700

    Update cektrgrpc.sh

--- cektrgrpc.sh
+++ cektrgrpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/IzhanV
+# My Telegram : https://t.me/gdstore2128
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -18,7 +18,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/rare/xray/grpc/trojangrpc.json | grep '^###' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/trojangrpc.json | grep '^###' | cut -d ' ' -f 2`);
 echo "-----------------------------------------";
 echo "---------=[ Trojan Grpc Login ]=---------";
 echo "-----------------------------------------";
@@ -55,5 +55,5 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "-----------------------------------------"
-echo "Script By Izhanworks"
+echo "Script By GDStore"
 rm -rf /tmp/other.txt
