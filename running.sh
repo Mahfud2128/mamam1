5b260e611d4030addebabe38fbfcb8cba0516089
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-08-13 08:13:14 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS http://griya-digital.my.id:81/ipvps | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Mahfud2128/access/main/ipvps | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
